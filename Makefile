7859c0d3cd59750509ff1c3f0dc0f67b74fcf54e
s3000 <s3000@D-6613026.s3000>
2025-07-04 17:25:54 +0200

    homework 5

---
+++ Makefile
@@ -0,0 +1,4 @@
+check:
+	poetry run black .
+	poetry run isort .
+	poetry run flake8 .
---
+++ test_moego_code.py
@@ -0,0 +1,13 @@
+def ya_ivan():
+    print("да я правда Иван")
+
+
+ya_ivan()
+
+
+def random_chislo():
+    for i in range(5):
+        print(i)
+
+
+random_chislo()
